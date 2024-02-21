5fad88897e847a29d86955345c7f3a4b26e839b7
ScottNC <scottnc@hotmail.co.uk>
2024-02-21 17:20:35 +0000

    changes multiple backgrounds avoiding text colours

--- src/background.js
+++ src/background.js
@@ -1,4 +1,4 @@
 
 export function changeBackground(html, oldColour, newColour) {
-    return html.replace(oldColour, newColour);
+    return html.replace(new RegExp("background-color: " + oldColour , 'g'), "background-color: " + newColour);
 }
--- test/background.test.js
+++ test/background.test.js
@@ -2,9 +2,57 @@ import {changeBackground} from '../src/background';
  
 describe('test background change', () => {
 
-    const div1 = "<div style=\"background-color: #FFFFFF;\">\n  Hello\n<\div>";
+    const div1 = `
+      <div style="background-color: #FFFFFF;">
+        Hello
+      </div>`
+      ;
 
-    test("background changes", () => {
-        expect(changeBackground(div1, "#FFFFFF", "#FF0000")).toBe("<div style=\"background-color: #FF0000;\">\n  Hello\n<\div>");
+    const res1 = `
+      <div style="background-color: #FF0000;">
+        Hello
+      </div>`
+      ;
+
+    test("changes background from white to red", () => {
+        expect(changeBackground(div1, "#FFFFFF", "#FF0000")).toBe(res1);
+    });
+
+    const div2 = `
+      <div style="background-color: #000000;">
+        Hello
+      </div>
+      <div style="background-color: #000000;">
+        There
+      </div>`
+      ;
+
+    const res2 = `
+      <div style="background-color: #0000FF;">
+        Hello
+      </div>
+      <div style="background-color: #0000FF;">
+        There
+      </div>`
+      ;
+
+    test("changes multiple backgrounds", () => {
+        expect(changeBackground(div2, "#000000", "#0000FF")).toBe(res2);
+    });
+
+    const div3 = `
+      <div style="background-color: #000000; color: #000000">
+        Hello
+      </div>
+      `;
+
+    const res3 = `
+      <div style="background-color: #0000FF; color: #000000">
+        Hello
+      </div>
+      `;
+
+    test("changes background colour and not text colour", () => {
+        expect(changeBackground(div3, "#000000", "#0000FF")).toBe(res3);
     });
 });
