aca861afaa6e48e203dd2e130fab5061cd964793
ScottNC <scottnc@hotmail.co.uk>
2024-02-22 13:39:45 +0000

    removes outer div layer

--- src/flattener.js
+++ src/flattener.js
@@ -1,3 +1,18 @@
+const { JSDOM } = require('jsdom');
+
 export function flatten(html) {
-    return html;
+
+    const dom = new JSDOM(html);
+    const body = dom.window.document.body;
+
+    const divElements = body.querySelectorAll('div');
+
+    divElements.forEach((div) => {
+        if (div.childElementCount === 1 && div.children[0].tagName === 'DIV') {
+            const child = div.children[0].cloneNode(true);
+            div.parentNode.replaceChild(child, div);
+        }
+    });
+
+    return body.innerHTML;
 }
--- test/flattener.test.js
+++ test/flattener.test.js
@@ -1,4 +1,5 @@
 import {flatten} from '../src/flattener';
+import { reformatHtml } from './helpers/testfunctions';
  
 describe.only('test div flattener', () => {
     const div1 = `
@@ -14,6 +15,24 @@ describe.only('test div flattener', () => {
       ;
 
     test("changes background from white to red", () => {
-        expect(flatten(div1)).toBe(res1);
+        expect(reformatHtml(flatten(div1))).toBe(reformatHtml(res1));
+    });
+
+    const div2 = `
+      <div>
+      <div style="background-color: #FFFFFF;">
+        Hello
+      </div>
+      </div>`
+      ;
+
+    const res2 = `
+      <div style="background-color: #FFFFFF;">
+        Hello
+      </div>`
+      ;
+
+    test("changes background from white to red", () => {
+        expect(reformatHtml(flatten(div2))).toBe(reformatHtml(res2));
     });
 });
