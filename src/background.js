8891c9b053bb961af8dfe7b96f3462927983c8e3
Esther Yekini <esther.yekini@force24.co.uk>
2024-02-23 16:19:01 +0000

    background color, font, mostly logo

--- src/background.js
+++ src/background.js
@@ -1,4 +1,42 @@
+const fs = require("fs");
+const jsdom = require('jsdom')
+const { JSDOM } = jsdom
 
-export function changeBackground(html, oldColour, newColour) {
-    return html.replace(new RegExp("background-color: " + oldColour , 'g'), "background-color: " + newColour);
+// const filepath = `src/microsite.html`
+
+function updateHtmlContent(filepath, updates) {
+  const data = fs.readFileSync(filepath, "utf8");
+  const dom = new JSDOM(data);
+  const document = dom.window.document;
+//   console.log(data.includes(updates.colors.background), '<-- STYLE')
+
+  // Update colors
+  if (updates.colors) {
+    const elements = document.querySelectorAll(updates.colors.background);
+    elements.forEach((element) => {
+      element.style.backgroundColor = updates.colors.color;
+    });
+  }
+
+  // Update fonts
+  if (updates.fonts && data.includes(updates.fonts.selector)) {
+    const elements = document.querySelectorAll(updates.fonts.selector);
+    elements.forEach((element) => {
+      element.style.fontFamily = updates.fonts.fontFamily;
+    });
+  }
+
+  // Update logos
+  if (updates.logos && data.includes(updates.logos.selector)) {
+    const logoElement = document.querySelector(updates.logos.selector);
+    if (logoElement) {
+      logoElement.src = updates.logos.newLogoUrl;
+    }
+  }
+
+  const updatedHtml = document.body.innerHTML;
+  console.log(updatedHtml, '<-- UPDATED HTML')
+  return updatedHtml;
 }
+
+module.exports = { updateHtmlContent }
--- test/background.test.js
+++ test/background.test.js
@@ -1,58 +1,56 @@
-import {changeBackground} from '../src/background';
- 
-describe('test background change', () => {
+const { updateHtmlContent } = require("../src/background.js");
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
 
-    const div1 = `
-      <div style="background-color: #FFFFFF;">
-        Hello
-      </div>`
-      ;
+describe("test snippet change", () => {
+  const updates = {
+    colors: {
+      background: ".clearfix",
+      color: "rgb(224, 70, 59)",
+    },
+    fonts: {
+      selector: "p",
+      fontFamily: "Fantasy, sans-serif",
+    },
+    logos: {
+      selector: ".logo",
+      newLogoUrl:
+        "https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png",
+    },
+  };
 
-    const res1 = `
-      <div style="background-color: #FF0000;">
-        Hello
-      </div>`
-      ;
+  test("changes background from blue to red", () => {
+    const updatedHtmlContent = updateHtmlContent(`src/microsite.html`, updates);
+    const dom = new JSDOM(updatedHtmlContent);
+    const document = dom.window.document;
+    const updatedElement = document.querySelector(".clearfix");
 
-    test("changes background from white to red", () => {
-        expect(changeBackground(div1, "#FFFFFF", "#FF0000")).toBe(res1);
-    });
+    expect(updatedElement.style.backgroundColor).toBe("rgb(224, 70, 59)");
+  });
+  test("changes font from open sans to fantasy", () => {
+    const updatedHtmlContent = updateHtmlContent(`src/microsite.html`, updates);
+    const dom = new JSDOM(updatedHtmlContent);
+    const document = dom.window.document;
+    const updatedElement = document.getElementsByTagName("p");
 
-    const div2 = `
-      <div style="background-color: #000000;">
-        Hello
-      </div>
-      <div style="background-color: #000000;">
-        There
-      </div>`
-      ;
+    expect(updatedElement.length).toBeGreaterThan(0);
+    if (updatedElement.length > 0) {
+      const computedStyle = dom.window.getComputedStyle(updatedElement[0]);
+      expect(computedStyle.fontFamily).toContain("Fantasy");
+    }
+  });
+  test("changes logo src", () => {
+    const updatedHtmlContent = updateHtmlContent(`src/microsite.html`, updates);
+    const dom = new JSDOM(updatedHtmlContent);
+    const document = dom.window.document;
+    const updatedElement = document.querySelectorAll(".logo");
 
-    const res2 = `
-      <div style="background-color: #0000FF;">
-        Hello
-      </div>
-      <div style="background-color: #0000FF;">
-        There
-      </div>`
-      ;
-
-    test("changes multiple backgrounds", () => {
-        expect(changeBackground(div2, "#000000", "#0000FF")).toBe(res2);
-    });
-
-    const div3 = `
-      <div style="background-color: #000000; color: #000000">
-        Hello
-      </div>
-      `;
-
-    const res3 = `
-      <div style="background-color: #0000FF; color: #000000">
-        Hello
-      </div>
-      `;
-
-    test("changes background colour and not text colour", () => {
-        expect(changeBackground(div3, "#000000", "#0000FF")).toBe(res3);
-    });
+    expect(updatedElement.length).toBeGreaterThan(0);
+    if (updatedElement.length > 0) {
+      const elementSrc = updatedElement[0].getAttribute("src");
+      expect(elementSrc).toBe(
+        "https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png"
+      );
+    }
+  });
 });
