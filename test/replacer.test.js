aae8ed263f9597137e2ba7cd6624f3d031834138
ScottNC <scottnc@hotmail.co.uk>
2024-02-24 13:00:14 +0000

    can extract f24 ids

--- test/replacer.test.js
+++ test/replacer.test.js
@@ -20,4 +20,17 @@ describe('test id replacer', () => {
         expect(reformatHtml(replaceId(div1))).toBe(reformatHtml(res1));
     });
 
+    // const div2 = `
+    // <div style="text-align: center;">&nbsp;</div>
+    // <div align="center"><img src="" alt="Image" name="Image1" border="0" data-f24-editable="" data-f24-display-name="ImageName" data-f24-src-source-type="file" data-f24-id="fc76a7bd-0640-4690-a57b-facadddc3e4c"></div>
+    // <div align="center"><img src="" alt="Image" name="Image1" border="0" data-f24-editable="" data-f24-display-name="ImageName" data-f24-src-source-type="file" data-f24-id="fc76a7bd-0640-4690-a57b-facaddd11111"></div>
+    // <div>&nbsp;</div>
+    //   `;
+
+    // const res2 = `
+    // <div style="text-align: center;">&nbsp;</div>
+    // <div align="center"><img src="" alt="Image" name="Image1" border="0" data-f24-editable="" data-f24-display-name="ImageName" data-f24-src-source-type="file" data-f24-id="fc76a7bd-0640-4690-a57b-facadddc3e4c"></div>
+    // <div align="center"><img src="" alt="Image" name="Image1" border="0" data-f24-editable="" data-f24-display-name="ImageName" data-f24-src-source-type="file" data-f24-id="fc76a7bd-0640-4690-a57b-facaddd11111"></div>
+    // <div>&nbsp;</div>
+    //   `;
 });
