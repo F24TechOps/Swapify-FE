0013a67717707c55686ad58383e2888fa5f16ff6
ScottNC <scottnc@hotmail.co.uk>
2024-02-24 18:15:38 +0000

    duplicate ids change to new uuid

--- src/replacer.js
+++ src/replacer.js
@@ -1,3 +1,26 @@
+import { extractId } from "./extractor";
+const { JSDOM } = require('jsdom');
+import { v4 as uuidv4 } from 'uuid';
+
 export function replaceId(html) {
-    return html;
+    const ids = new Set(...[extractId(html)]);
+    return Array.from(ids).reduce(replaceIdsWithValues, html);
+}
+
+function replaceIdsWithValues (html, id) {
+    const dom = new JSDOM(html);
+    const document = dom.window.document;
+
+    const f24IdElements = Array.from(document.querySelectorAll('[data-f24-id]'))
+        .filter((element) => element.getAttribute('data-f24-id') === id);
+
+    if (f24IdElements.length <= 1)
+        return html;
+
+    f24IdElements.forEach((element, idx) => {
+        if (idx)
+            element.setAttribute('data-f24-id', uuidv4())
+    });
+
+    return dom.serialize();
 }
