6994d769265376524db4f57d61ab455dcdc31853
ScottNC <scottnc@hotmail.co.uk>
2024-03-06 17:26:20 +0000

    updates all buttons of certain type

--- src/update.js
+++ src/update.js
@@ -1,8 +1,8 @@
 import { readAndRun, readFile } from "./runAll.js";
 
-const jsonData = readFile('./src/json/microsite.json', 'utf8');
+const jsonData = readFile('./src/json/microsite2.json', 'utf8');
 const update = JSON.parse(jsonData);
 
 const selections = {replaceId: true, flatten: true, update};
 
-readAndRun('./src/html/microsite.html', `src/html/updatedmicrosite${Date.now()}.html`, selections);
+readAndRun('./src/html/microsite2.html', `src/html/updatedmicrosite${Date.now()}.html`, selections);
