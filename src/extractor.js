a3124965512a2ebb2999ab8c316e8eba9633595b
Esther Yekini <esther.yekini@force24.co.uk>
2024-03-12 16:00:24 +0000

    email buttons

--- src/extractor.js
+++ src/extractor.js
@@ -1,50 +1,72 @@
-import { JSDOM } from 'jsdom';
+import { JSDOM } from "jsdom";
 
 export function extractId(html) {
-    const dom = new JSDOM(html);
-    const document = dom.window.document;
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
 
-    const f24IdElements = document.querySelectorAll('[data-f24-id]');
+  const f24IdElements = document.querySelectorAll("[data-f24-id]");
 
-    return Array.from(f24IdElements).map((element) => element.getAttribute('data-f24-id'));
+  return Array.from(f24IdElements).map((element) =>
+    element.getAttribute("data-f24-id")
+  );
 }
 
-export const extractBackgrounds = (html) => extractFeature(html, (element, dom) => dom.window.getComputedStyle(element, null).backgroundColor, ['rgba(0, 0, 0, 0)', 'inherit'], "div");
+export const extractBackgrounds = (html) =>
+  extractFeature(
+    html,
+    (element, dom) =>
+      dom.window.getComputedStyle(element, null).backgroundColor,
+    ["rgba(0, 0, 0, 0)", "inherit"],
+    "div"
+  );
 
-export const extractFonts = (html) => extractFeature(html, (element, dom) => dom.window.getComputedStyle(element, null).fontFamily, [], "div");
+export const extractFonts = (html) =>
+  extractFeature(
+    html,
+    (element, dom) => dom.window.getComputedStyle(element, null).fontFamily,
+    [],
+    "div"
+  );
 
-export const extractImage = (html) => extractFeature(html, (element) => element.src, [], "img");
+export const extractImage = (html) =>
+  extractFeature(html, (element) => element.src, [], "img");
 
 function extractFeature(html, getFeature, nonExistent, tagName) {
-    const dom = new JSDOM(html);
-    const document = dom.window.document;
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
 
-    const allElements = Array.from(document.getElementsByTagName(tagName));
+  const allElements = Array.from(document.getElementsByTagName(tagName));
 
-    const allBackgrounds = allElements.map((element) => getFeature(element, dom))
-    
-    return Array.from(new Set(...[allBackgrounds])).filter(a => !nonExistent.includes(a));
+  const allBackgrounds = allElements.map((element) => getFeature(element, dom));
+
+  return Array.from(new Set(...[allBackgrounds])).filter(
+    (a) => !nonExistent.includes(a)
+  );
 }
 
 export const getButtonInfo = (element) => {
-    const styles = element.getAttribute('style').split(';').filter(str => str.includes(':')).sort();
-    const styleObject = {};
+  const styles = element
+    .getAttribute("style")
+    .split(";")
+    .filter((str) => str.includes(":"))
+    .sort();
+  const styleObject = {};
 
-    styles.forEach((style) => {
-        const [key, value] = style.split(':').map(prop => prop.trim());
-        styleObject[key] = value;
-    });
+  styles.forEach((style) => {
+    const [key, value] = style.split(":").map((prop) => prop.trim().replace("\n", ""));
+    styleObject[key] = value.replace(/\s+/g, '');
+  });
 
-    return JSON.stringify(styleObject, null, 2);
+  return JSON.stringify(styleObject, null, 2);
 };
 
 export function extractButton(html) {
-    const dom = new JSDOM(html);
-    const document = dom.window.document;
+  const dom = new JSDOM(html);
+  const document = dom.window.document;
 
-    const allElements = Array.from(document.getElementsByClassName("btn"));
+  const allElements = Array.from(document.getElementsByClassName("btn"));
 
-    const allStyles = allElements.map(getButtonInfo);
+  const allStyles = allElements.map(getButtonInfo);
 
-    return Array.from(new Set(...[allStyles]));
+  return Array.from(new Set(...[allStyles]));
 }
