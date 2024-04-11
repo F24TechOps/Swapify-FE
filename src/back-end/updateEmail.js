3c5dc14a640f94aa8614321acc949d57740af831
ScottNC <scottnc@hotmail.co.uk>
2024-04-11 10:39:37 +0100

    fix merge

--- src/back-end/updateEmail.js
+++ src/back-end/updateEmail.js
@@ -5,8 +5,4 @@ const update = JSON.parse(jsonData);
 
 const selections = {replaceId: false, flatten: true, update};
 
-<<<<<<< HEAD
 readAndRun('./.env/original.html', `./.env/force/template.html`, selections, 'email');
-=======
-readAndRun('./src/html/email2.html', `src/html/updatedemail3.html`, selections, 'email');
->>>>>>> 2bccceb (delete unused html)
