44f34302baf7a52717dff5173dc2dd1355738442
ScottNC <scottnc@hotmail.co.uk>
2024-04-11 10:39:37 +0100

    fix merge

--- src/back-end/createMap.js
+++ src/back-end/createMap.js
@@ -5,8 +5,4 @@ const html = readFile('./src/html/email.html');
 
 const mapping = await createMapping(html, 'email');
 
-<<<<<<< HEAD
 writeFile('./src/json/microsite4.json', JSON.stringify(mapping, null, 2));
-=======
-writeFile('./src/json/email2.json', JSON.stringify(mapping, null, 2));
->>>>>>> 2bccceb (delete unused html)
--- src/back-end/updateEmail.js
+++ src/back-end/updateEmail.js
@@ -1,14 +1,8 @@
 import { readAndRun, readFile } from "./runAllMicrosite.js";
 
-const companyName = 'force24';
-
 const jsonData = readFile('./.env/original.json', 'utf8');
 const update = JSON.parse(jsonData);
 
 const selections = {replaceId: false, flatten: true, update};
 
-<<<<<<< HEAD
 readAndRun('./.env/original.html', `./.env/force/template.html`, selections, 'email');
-=======
-readAndRun('./src/html/email2.html', `src/html/updatedemail3.html`, selections, 'email');
->>>>>>> 2bccceb (delete unused html)
