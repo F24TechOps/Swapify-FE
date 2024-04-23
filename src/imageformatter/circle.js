7d6e39d8765ccaeb627448265c8c857391e45d23
ScottNC <scottnc@hotmail.co.uk>
2024-04-23 14:55:11 +0100

    creates email zip file

--- src/imageformatter/circle.js
+++ src/imageformatter/circle.js
@@ -1,9 +1,9 @@
 import { cropCircle } from './createCircle.js';
 
 const company = process.argv[2] ?? 'force';
-const imageName = process.argv[3] ?? 'portrait';
 
-const inputFile = `./.env/${company}/email/final/images/${imageName}.png`;
+const inputFile = process.argv[3] ?? `./.env/${company}/email/final/images/portrait.png`;
+
 const outputFile = `./.env/${company}/email/final/images/circle.png`;
 
 cropCircle(inputFile, outputFile);
