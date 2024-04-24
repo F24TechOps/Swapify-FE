90382c0bd19e0511f092ce074317e8274d1e5940
Esther Yekini <esther.yekini@force24.co.uk>
2024-04-24 16:05:41 +0100

    changed file path

--- src/back-end/checkHtml.js
+++
@@ -1,3 +0,0 @@
-export function isFullHtml(html) {
-    return /^<html[\s>]/i.test(html.trim()) || /^<!DOCTYPE html>\s*<html[\s>]/i.test(html.trim());
-}
--- src/back-end/updateMicrosite.js
+++
@@ -1,8 +0,0 @@
-import { readAndRun, readFile } from "./runAll.js";
-
-const jsonData = readFile('./.env/json/microsite.json', 'utf8');
-const update = JSON.parse(jsonData);
-
-const selections = {replaceId: true, flatten: true, update};
-
-readAndRun('../swapify-frontend/html/microsite/base1/template.html', `.env/microsite.html`, selections, 'microsite');
