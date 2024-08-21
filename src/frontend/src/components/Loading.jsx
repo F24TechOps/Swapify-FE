abf75de0447b5e468301abf0b80c7bcf1cffa1f3
Esther Yekini <esther.yekini@force24.co.uk>
2024-08-21 10:43:33 +0100

    loading animation

--- src/frontend/src/components/Loading.jsx
+++ src/frontend/src/components/Loading.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
-import '../css/body.css';
+import React from "react";
+import "../css/body.css";
 
 function Loading() {
   return (
-    <div className="loading-container">
-      <div className="spinner"></div>
-      <div className="loading-text">Loading...</div>
+    <div className="wrapper">
+      <svg>
+        <text x="50%" y="50%" dy=".35em" text-anchor="middle">
+          SWAP
+        </text>
+      </svg>
     </div>
   );
 }
